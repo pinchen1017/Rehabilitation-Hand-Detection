af03d23c23dcb3063d69b7a057d27edcb8f87e82
Risto8456 <risto13579@gmail.com>
2025-12-19 16:42:01 +0800

    add: Add data and models 增加資料與模型

--- src/config.py
+++ src/config.py
@@ -47,7 +47,7 @@ DEFAULT_MAX_HANDS = 1
 SMOOTHER_WINDOW_SIZE = 5
 
 # 模型路徑
-DEFAULT_MODEL_PATH = "models/rehab_action_classifier_clear_8_332301.h5"
+DEFAULT_MODEL_PATH = "models/enhanced_model.h5"
 
 # UI 設定
 FONT_SCALE = 0.8
